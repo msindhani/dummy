772234461262cad3b87e89dc84dc3490b7c38421
msindhani <46098861+msindhani@users.noreply.github.com>
2019-06-12 10:50:57 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,28 @@
+
+
+#List for Enquiries
+crops=["apple","cotton","grapes","walnut","pomogrenate","mango", "मिर्च","banana","orange","onion","peanut","rice","tea","सेब","अंगूर","अखरोट","अनार","आम"]
+
+diseases=["premature", "alternaria", "blight", "leaf", "spot", "fall", "echinochloa", "cynodon", "dactylon","digitaria", "marginata","dactylocteneum", 
+         "aegyptium","jassid","whitefly","powdery", "mildew","downey", "scab","rust", "husk","thrips","cancar", "root","rot","sigatoka","marssonina","sigatoka",
+          "cholartic","treatment","disease","cure","cured","marssonina","mosaic","solution","rusting","echinochloa"]
+
+products = ["cabrio", "priaxor", "basta", "safina",	"acrisio","merivon", "insecticide", "pesticide", "fungicide","pollinator","herbicide", "fungal",
+            "reglis plus","opera","verismo","intrepid","lihocin","headline","zampro","stomp","dosage","dose","spray","interval","frequency","schedule","मेरिवॉन",
+            "कॅब्रियो","टॉप","सफ़ीना","बस्ता","time","constituent","combination","technical","composition","मात्रा","contains","area","mancozeb"]
+
+product_Dosage=['dose',"spray","interval","frequency","schedule","time","stage"]
+
+product_Technical=["constituent","combination","technical","composition","मात्रा","contains"]
+
+# List for Complaint
+
+expensive=['expensive', 'costly', 'price', 'rate', 'affordable']
+competitors=['avtar', 'ergon', 'movento', 'energy', 'nativo', 'antracol', 'tata', 'syngenta', 'bumper', 'chemtop', 'cheminova', 'difenoconazole', 'noor', 'acrisio',
+             'indofil','formica','adama','base','india','rely','tilt','taspa', 'agrobooster']
+
+#List of products
+product_Love = ["cabrio", "priaxor", "basta", "safina",	"acrisio","merivon", "insecticide", "pesticide", "fungicide","pollinator","herbicide", "fungal",
+            "reglis plus","opera","verismo","intrepid","lihocin","headline","zampro","stomp","मेरिवॉन","कॅब्रियो","टॉप","सफ़ीना","बस्ता"]
+
+suggestion=['price','expensive','costly','cost','rate']
